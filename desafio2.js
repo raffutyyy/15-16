3c1a836c2bd700bf14b39caad9f7df7139b05e49
Raffutyyy.Raffaela <Raffa.fdo@gmail.com>
2024-10-22 14:31:15 -0300

    Aula 17/10

--- desafio2.js
+++ desafio2.js
@@ -1,25 +1,80 @@
-let nome = prompt('Nome completo:')
-let tipoJogo = prompt('Tipo de jogo(IN para internacional e DO para doméstico):')
-let etapa = prompt('Etapa do jogo(SF p/ semi-final; DT p/ decisão terceiro lugar; FI p/ final):')
-let categoria = prompt('Categoria(1,2,3 ou 4):')
-let quantidade = prompt('Quantidade de ingressos:')
+function jogos() {
+    let nome = prompt('Digite seu nome completo:')
+    let tipo = prompt('Digite o tipo de jogo, IN(internacional) e DO (Doméstico):')
+    let etapa = prompt('Digite a etapa do jogo, SF(semi-final),  DT (decisão de 3º lugar) ou FI (final):')
+    let categoria = Number(prompt('Digite a categoria do ingresso (1, 2, 3 ou 4):'))
+    const quantidade = Number(prompt('Digite a quantidade de ingressos:'));
 
-function Jogos(nome, tipo, etapa, categoria, quantidade) {
-    let valorIngresso = Number(categoria)
-    console.log(`---Dados da compra---`)
+    let preco
+    let dolar = 4.10
+
+    if (etapa === "SF") {
+        switch (categoria) {
+            case 1:
+                preco = 1320
+                break
+            case 2:
+                preco = 880
+                break
+            case 3:
+                preco = 550
+                break
+            case 4:
+                preco = 220
+                break
+        }
+    } else if (etapa === 'DT') {
+        switch (categoria) {
+            case 1:
+                preco = 660
+                break
+            case 2:
+                preco = 440
+                break
+            case 3:
+                preco = 330
+                break
+            case 4:
+                preco = 170
+                break
+        }
+    } else if (etapa=== "FI") {
+        switch (categoria) {
+            case 1:
+                preco = 1980
+                break
+            case 2:
+                preco = 1320
+                break
+            case 3:
+                preco = 880
+                break
+            case 4:
+                preco = 330
+                break
+        }
+
+    } else {
+        console.log("Etapa do jogo inválida!")
+        return
+
+    }
+    if (tipo === "IN") {
+        preco *= dolar
+    }
+
+    let total = preco * quantidade
+
+    console.log('---Detalhes da compra---')
     console.log(`Nome do cliente: ${nome}`)
-    console.log(`Tipo do jogo: ${tipo}`)
-    console.log(`Etapa do jogo: ${etapa}`)
-    console.log(`Categoria: ${categoria}`)
-    console.log(`Quantidade de Ingressos: ${quantidade}`)
-   /* if (categoria === '1') {
-        valorIngresso = 
- } else {
+    console.log(`Tipo de jogo: ${tipo === 'IN' ? 'Internacional' : 'Doméstico'}`)
+    console.log(`Etapa do Jogo: ${etapa === "SF" ? "Semi-final" : etapa === "DT" ? "Decisão de 3º lugar" : "Final"}`);
+    console.log(`Categoria: ${categoria}`);
+    console.log(`Quantidade de Ingressos: ${quantidade}`);
+    console.log(`Valor do Ingresso: R$ ${preco.toFixed(2)}`);
+    console.log(`Valor Total: R$ ${total.toFixed(2)}`);
 
-    }*/
    
-
 }
 
-
-
+jogos()
--- ex008.js
+++ ex008.js
@@ -8,5 +8,5 @@ if(numero > 0) {
 console.log(mensagem)
 
 //A) criou uma varivel numero que pede para o usuario digitar um numerp
-//B) vai exibir a mensagem secreta
+//B) 10 -> esse numero passou no teste  -10 -> ocorre um erro
 //C) Sim, o console nao esta sendo chamado, para exibir a mensagem dentro do console seria necessario que fosse um return
